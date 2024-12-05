b88b14df4edbf351c6f5318ee8a0117da4bd56a8
Petar <pecka21@proton.me>
2024-12-05 21:47:32 +0100

    Created the home-page for now. Added also nav-links to Login and Admin. The page is not finished yet.

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,38 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <Button>Click</Button>
+    <div className="flex h-screen max-h-screen">
+      <nav className="absolute top-7 right-24 flex space-x-6">
+        <a href="/login" className="text-sm font-medium text-white">
+          Login
+        </a>
+        <a href="/?admin=true" className="text-sm font-medium text-orange-600">
+          Admin
+        </a>
+      </nav>
+
+      <section className="container my-auto remove-scrollbar">
+        <div className="max-w-[496px] sub-container">
+          <h1>PeakTrainer</h1>
+        </div>
+
+        <div className="max-w-[496px] sub-container">
+          <p>
+            Take charge of your training business with PeakTrainer. This
+            all-in-one app streamlines scheduling, client tracking, and session
+            management, helping you stay organized and focus on your success.
+          </p>
+          <Button className="bg-orange-600 text-white hover:bg-orange-500 mt-7">
+            Get Started
+          </Button>
+        </div>
+
+        <div className="max-w-[496px] sub-container text-14-regular">
+          <p className="text-dark-600 xl:text-left">
+            © 2024 PeakTrainer, All Rights Reserved
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
