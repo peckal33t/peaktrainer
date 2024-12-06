82126652bbae1459383206da192fe952e4e66bf1
Petar <pecka21@proton.me>
2024-12-06 18:22:01 +0100

    Created a register page and added my ClientForm component for now just so see so it works. It did work!

---
+++ app/register/page.tsx
@@ -0,0 +1,12 @@
+import ClientForm from "@/components/forms/ClientForm";
+import React from "react";
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <ClientForm />
+    </div>
+  );
+};
+
+export default RegisterPage;
