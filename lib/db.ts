13c4e47be1ab83bfd2469b38dd762b617510c414
Petar <pecka21@proton.me>
2024-12-10 14:00:40 +0100

    Setting up the database and reading the stuff from the database.

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,6 @@
-const {
+import * as sdk from "node-appwrite";
+
+export const {
   PROJECT_ID,
   API_KEY,
   DATABASE_ID,
@@ -8,3 +10,12 @@ const {
   NEXT_BUCKET_STORAGE: STORAGE,
   NEXT_ENDPOINT: ENDPOINT,
 } = process.env;
+
+const appwriteClient = new sdk.Client();
+
+appwriteClient.setKey(API_KEY!).setEndpoint(ENDPOINT!).setProject(PROJECT_ID!);
+
+export const appwriteDatabases = new sdk.Databases(appwriteClient);
+export const appwriteUsers = new sdk.Users(appwriteClient);
+export const appwriteStorage = new sdk.Storage(appwriteClient);
+export const appwriteMessaging = new sdk.Messaging(appwriteClient);
