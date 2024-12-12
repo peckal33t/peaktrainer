74cf911eb417a19d0b61da31af39757fb109f68b
Petar <pecka21@proton.me>
2024-12-12 16:54:47 +0100

    Adding the logo, nav and the footer from my home page to the register page.

--- app/clients/[userId]/register/page.tsx
+++ app/clients/[userId]/register/page.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+import Link from "next/link";
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <div className="flex h-screen max-h-screen">
+      <nav className="absolute top-7 right-24 flex space-x-6">
+        <Link href="/login" className="text-sm font-medium text-white">
+          Login
+        </Link>
+        <Link
+          href="/?admin=true"
+          className="text-sm font-medium text-orange-600"
+        >
+          Admin
+        </Link>
+      </nav>
+
+      <section className="container my-auto remove-scrollbar">
+        <div className="max-w-[496px] sub-container">
+          <Link href="/">
+            <h1 className="text-xl">
+              Peak<span className="text-orange-500">Trainer</span>
+            </h1>
+          </Link>
+        </div>
+
+        {/* <div className="max-w-[496px] sub-container">
+          <p>
+            Take charge of your training business with PeakTrainer. This
+            all-in-one app streamlines scheduling, client tracking, and session
+            management, helping you stay organized and focus on your success.
+          </p>
+          <br />
+        </div> */}
+
+        <div className="max-w-[496px] sub-container text-14-regular">
+          <p className="text-dark-600 xl:text-left">
+            © 2024 PeakTrainer, All Rights Reserved
+          </p>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default RegisterPage;
