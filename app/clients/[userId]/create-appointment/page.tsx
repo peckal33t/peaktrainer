56f104a42a8efcb01ad2bf7e50e54bb14bcfd87d
Petar <pecka21@proton.me>
2024-12-17 18:28:26 +0100

    Adding the stuff from the home page to the appointment page (nav, logo, footer).

--- app/clients/[userId]/create-appointment/page.tsx
+++ app/clients/[userId]/create-appointment/page.tsx
@@ -1,5 +1,37 @@
+import Link from "next/link";
+
 const AppointmentPage = () => {
-  return <div>AppointmentPage</div>;
+  return (
+    <div className="flex h-screen max-h-screen">
+      <nav className="absolute top-7 right-24 flex space-x-6">
+        <Link href="/login" className="text-sm font-medium text-white">
+          Login
+        </Link>
+        <Link
+          href="/?admin=true"
+          className="text-sm font-medium text-orange-600"
+        >
+          Admin
+        </Link>
+      </nav>
+
+      <section className="container my-auto remove-scrollbar">
+        <div className="max-w-[496px] sub-container">
+          <Link href="/">
+            <h1 className="text-xl">
+              Peak<span className="text-orange-500">Trainer</span>
+            </h1>
+          </Link>
+        </div>
+
+        <div className="max-w-[496px] sub-container text-14-regular">
+          <p className="text-dark-600 xl:text-left">
+            © 2024 PeakTrainer, All Rights Reserved
+          </p>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default AppointmentPage;
