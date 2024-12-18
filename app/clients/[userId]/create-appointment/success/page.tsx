ecdc3310b81f3685d955d2d9a60a42f406e1835f
Petar <pecka21@proton.me>
2024-12-18 19:11:56 +0100

    Just some text to show the client.

--- app/clients/[userId]/create-appointment/success/page.tsx
+++ app/clients/[userId]/create-appointment/success/page.tsx
@@ -1,5 +1,29 @@
+import Link from "next/link";
+
 const SuccessPage = () => {
-  return <div>SuccessPage</div>;
+  return (
+    <div className="flex h-screen max-h-screen px-[5%]">
+      <div className="success-img">
+        <Link href="/">
+          <h1 className="text-2xl">
+            Peak<span className="text-orange-500">Trainer</span>
+          </h1>
+        </Link>
+
+        <div className="flex flex-col items-center">
+          <h1 className="text-xl">
+            Your appointment has been{" "}
+            <span className="text-orange-500">successfully scheduled!</span> 🎉
+          </h1>
+          <p className="mt-2 text-dark-700">
+            {" "}
+            We’re excited to see you! You will receive a SMS-confirmation with
+            all the details shortly.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SuccessPage;
