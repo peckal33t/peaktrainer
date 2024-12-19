5866f8e51a909bdbaddf88f1673f4ab834526a3a
Petar <pecka21@proton.me>
2024-12-19 18:40:55 +0100

    Little bit of styling the admin dashboard and editing some global css properties.

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import Link from "next/link";
 
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  return (
+    <div className="mx-auto flex flex-col max-w-7xl space-y-4 py-5">
+      <div className="admin-header">
+        <Link href="/">
+          <h1 className="text-2xl">
+            Peak<span className="text-orange-500">Trainer</span>
+          </h1>
+        </Link>
+        <p>Admin</p>
+      </div>
+
+      <section className="admin-main">
+        <div className="w-full space-y-4">
+          <h1 className="text-xl">Admin Dashboard</h1>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default AdminPage;
