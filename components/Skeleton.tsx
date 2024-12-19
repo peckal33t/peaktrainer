ce91a360916827ae64b937dc5dbf27f21c8197cd
Petar <pecka21@proton.me>
2024-12-19 16:30:01 +0100

    Adding an X to the pop-up and created function to close it.

--- components/Skeleton.tsx
+++ components/Skeleton.tsx
@@ -12,9 +12,17 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import { X } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const Skeleton = () => {
+  const router = useRouter();
   const [isOpen, setIsOpen] = useState(true);
+
+  const closePopUp = () => {
+    setIsOpen(false);
+    router.push("/");
+  };
 
   return (
     <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
@@ -22,6 +30,12 @@ const Skeleton = () => {
         <AlertDialogHeader>
           <AlertDialogTitle className="flex items-start justify-between">
             Authorization Code
+            <X
+              width={20}
+              height={20}
+              className="cursor-pointer"
+              onClick={() => closePopUp()}
+            />
           </AlertDialogTitle>
           <AlertDialogDescription>
             This action cannot be undone. This will permanently delete your
