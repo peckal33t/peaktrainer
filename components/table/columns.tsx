0051dbcb52a34d6fdd780a136ea1bdd9b9c7d7b7
Petar <pecka21@proton.me>
2024-12-20 13:28:06 +0100

    Adding the stuff I need to my form to show information about a client.

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { formatDateTime } from "@/lib/utilities";
 import { Appointment } from "@/types/db.types";
+import { Trainers } from "@/variables/variables";
 import { ColumnDef } from "@tanstack/react-table";
 
 // This type is used to define the shape of our data.
@@ -10,6 +12,12 @@ export type Payment = {
   amount: number;
   status: "pending" | "processing" | "success" | "failed";
   email: string;
+};
+
+const statusColors: Record<string, string> = {
+  scheduled: "text-green-500",
+  pending: "text-blue-500",
+  cancelled: "text-red-500",
 };
 
 export const columns: ColumnDef<Appointment>[] = [
@@ -22,6 +30,14 @@ export const columns: ColumnDef<Appointment>[] = [
     },
   },
   {
+    accessorKey: "phone",
+    header: "Phone",
+    cell: ({ row }) => {
+      const appointment = row.original;
+      return <p>{appointment.client.phone}</p>;
+    },
+  },
+  {
     accessorKey: "email",
     header: "Email",
     cell: ({ row }) => {
@@ -30,11 +46,37 @@ export const columns: ColumnDef<Appointment>[] = [
     },
   },
   {
+    accessorKey: "statusType",
+    header: "Status",
+    cell: ({ row }) => {
+      const appointment = row.original;
+      const statusColor =
+        statusColors[appointment.statusType] || "text-gray-500";
+      return (
+        <p className={`capitalize ${statusColor}`}>{appointment.statusType}</p>
+      );
+    },
+  },
+  {
+    accessorKey: "appointmentDate",
+    header: "Appointment Date",
+    cell: ({ row }) => {
+      const appointment = row.original;
+      return <p>{formatDateTime(appointment.appointmentDate).dateTime}</p>;
+    },
+  },
+  {
     accessorKey: "primaryTrainer",
     header: "Trainer",
     cell: ({ row }) => {
-      const appointment = row.original;
-      return <p>PT. {appointment.client.primaryTrainer}</p>;
+      const trainer = Trainers.find(
+        (pt) => pt.name === row.original.trainerName
+      );
+      return (
+        <p>
+          <span className="text-orange-500">PT</span>. {trainer?.name}
+        </p>
+      );
     },
   },
 ];
