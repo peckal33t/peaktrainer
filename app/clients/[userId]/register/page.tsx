acdb3c16970bb0b3622ea41d70489a1722ac7b3c
Petar <pecka21@proton.me>
2024-12-25 02:43:10 +0100

    Removing the login from the nav-menu.

--- app/clients/[userId]/register/page.tsx
+++ app/clients/[userId]/register/page.tsx
@@ -10,9 +10,6 @@ const RegisterPage = async ({ params }: SearchParamProps) => {
   return (
     <div className="flex h-screen max-h-screen">
       <nav className="absolute top-7 right-24 flex space-x-6">
-        <Link href="/login" className="text-sm font-medium text-white">
-          Login
-        </Link>
         <Link
           href="/?admin=true"
           className="text-sm font-medium text-orange-600"
--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,6 @@ export default async function Home({ searchParams }: SearchParamProps) {
       {admin && <Skeleton />}
 
       <nav className="absolute top-7 right-24 flex space-x-6">
-        <Link href="/login" className="text-sm font-medium text-white">
-          Login
-        </Link>
         <Link
           href="/?admin=true"
           className="text-sm font-medium text-orange-600"
