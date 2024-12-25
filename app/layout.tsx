a30ef5b22f60fa0983b19edbc4f79caf94173889
Petar <pecka21@proton.me>
2024-12-25 17:24:21 +0100

    Adding my icon to the app.

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ export const metadata: Metadata = {
   title: "PeakTrainer",
   description:
     "A personal trainer management app designed to streamline scheduling, client tracking, and session management.",
+  icons: {
+    icon: "/favicon-32x32.png",
+  },
 };
 
 export default function RootLayout({
